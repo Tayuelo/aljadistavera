699573adfc3ac0c0eb510660b3f2ded1dede52f7
Aljadis Tavera <aljadis.tavera@fairwaymc.com>
2022-08-07 12:28:38 -0500

    Avance portafolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BioPageComponent } from './pages/bio-page/bio-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'bio',
+    component: BioPageComponent
+  },
+  {
+    path: 'skills',
+    component: SkillsPageComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsPageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { BioPageComponent } from './pages/bio-page/bio-page.component';
+import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    BioPageComponent,
+    SkillsPageComponent,
+    ProjectsPageComponent,
+    SidebarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
