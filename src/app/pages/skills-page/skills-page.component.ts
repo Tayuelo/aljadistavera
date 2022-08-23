22cf7ef63bd0fa59739f1ecd94156bfc8b0bb374
Tayuelo <tayuelo26@gmail.com>
2022-08-22 23:27:12 -0500

    Skills page

--- src/app/pages/skills-page/skills-page.component.ts
+++ src/app/pages/skills-page/skills-page.component.ts
@@ -7,6 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsPageComponent implements OnInit {
 
+  public skills = [
+    {
+      logo: 'assets/angular_logo.png',
+      name: 'Angular',
+      rate: '100%'
+    },
+    {
+      logo: 'assets/nodejs_logo.png',
+      name: 'Node.js',
+      rate: '100%'
+    },
+    {
+      logo: 'assets/ionic_logo.png',
+      name: 'Ionic Framework',
+      rate: '100%'
+    },
+    {
+      logo: 'assets/html_logo.png',
+      name: 'HTML',
+      rate: '100%'
+    },
+    {
+      logo: 'assets/css_logo.png',
+      name: 'CSS',
+      rate: '100%'
+    },
+    {
+      logo: 'assets/js_logo.png',
+      name: 'JavaScript',
+      rate: '100%'
+    },
+
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
