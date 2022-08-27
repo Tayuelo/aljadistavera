bb273d9f62dfbd35677c5b41a83213f2361f167c
Tayuelo <tayuelo26@gmail.com>
2022-08-27 16:13:59 -0500

    Deployment to heroku

--- src/app/pages/skills-page/skills-page.component.ts
+++ src/app/pages/skills-page/skills-page.component.ts
@@ -9,32 +9,32 @@ export class SkillsPageComponent implements OnInit {
 
   public skills = [
     {
-      logo: 'assets/angular_logo.png',
+      logo: 'static/angular_logo.png',
       name: 'Angular',
       rate: '100%'
     },
     {
-      logo: 'assets/nodejs_logo.png',
+      logo: 'static/nodejs_logo.png',
       name: 'Node.js',
       rate: '100%'
     },
     {
-      logo: 'assets/ionic_logo.png',
+      logo: 'static/ionic_logo.png',
       name: 'Ionic Framework',
       rate: '100%'
     },
     {
-      logo: 'assets/html_logo.png',
+      logo: 'static/html_logo.png',
       name: 'HTML',
       rate: '100%'
     },
     {
-      logo: 'assets/css_logo.png',
+      logo: 'static/css_logo.png',
       name: 'CSS',
       rate: '100%'
     },
     {
-      logo: 'assets/js_logo.png',
+      logo: 'static/js_logo.png',
       name: 'JavaScript',
       rate: '100%'
     },
